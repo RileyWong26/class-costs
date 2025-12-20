ddabca37672e98f39d0caca11a9f2147e3cb9f29
RileyWong26 <rileywongwong@gmail.com>
2025-12-19 22:16:42 -0500

    Supabase client

---
+++ Backend/supabase.ts
@@ -0,0 +1,25 @@
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
+import 'dotenv/config';
+import { Program } from "./parser.js";
+
+const supabaseUrl = process.env.SUPABASE_URL;
+const supabaseKey = process.env.SUPABASE_KEY;
+if (!supabaseKey || !supabaseUrl){
+    throw new Error("URL or KEY is invalid");
+}
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+export async function insertProgramRows(Table: string, program: Array<Program>){
+    const {data, error} = await supabase
+    .from(Table)
+    .insert(program)
+    
+    if (error) {
+        console.log("Error " + error.message);
+    }
+    else{
+        console.log("Inserted");
+    }
+}
+
+export default supabase;
