cd028ba5e35c1bd9f7a26247abe9e111adea1d68
RileyWong26 <rileywongwong@gmail.com>
2026-01-08 23:33:23 -0500

    Fixed an issue that involved setting every tuition cost to be the same in the tuition cost parser

--- Backend/parser.ts
+++ Backend/parser.ts
@@ -14,6 +14,7 @@ export class Program {
     Y3O: number;
     Y4O: number;
     Y5O: number;
+
     constructor(Name: string, Y1: number, Y2: number, Y3: number, Y4: number, Y5: number, Y1O: number, Y2O: number, Y3O: number, Y4O: number, Y5O: number,){
         this.Name = Name;
         this.Y1 = Y1;
@@ -26,6 +27,12 @@ export class Program {
         this.Y3O = Y3O;
         this.Y4O = Y4O;
         this.Y5O = Y5O;
+        if (Y5 == 0){
+            this.Y5 = Y4;
+        }
+        if (Y5O == 0){
+            this.Y5O = Y4O;
+        }
     }
 }
 
@@ -48,7 +55,7 @@ function findProgram(name: string) : Program | undefined {
     return p;
 }
 
-// Set the values of the program
+// Set the values of the program tuition
 function setProgram(program: Program, name : string, y1: number, y2: number, y3: number, y4: number, y5: number, y1O: number, y2O: number, y3O: number, y4O: number, y5O: number){
     if (y1 != 0 && program.Y1 == 0){
         program.Y1 = y1;
@@ -69,6 +76,10 @@ function setProgram(program: Program, name : string, y1: number, y2: number, y3:
     if (y5 != 0 && program.Y5 == 0){
         program.Y5 = y5;
         program.Y5O = y5O;
+    }
+    if (y5 == 0 && program.Y5 == 0){
+        program.Y5 = program.Y4;
+        program.Y5O = program.Y4O;
     }
 }
 // Parse pdf
@@ -109,58 +120,67 @@ result.pages.forEach((page) => {
         
         // Parse if it contains the word year
         if (containsYear(line)){
+            // Break the line into an array
             const row: Array<string> = line.split(" ");
+            // Word position
             var pos : number = 0;
 
-            // Name of program
-            while (row[pos] !== "Upper" && row[pos]?.toLowerCase() !== "year" && row[pos]?.toLowerCase() !== "years" && row[pos] !== "All") {
-                if (flag5 || flag4 || flag3 || flag2 || flag1){
-                    // Unique program, does not already exist, so create a new program and push
-                    const program: Program | undefined = findProgram(Name.trim());
-                    if (program === undefined){
-                        const p = new Program(Name.trim(), y1, y2, y3, y4, y5, y1O, y2O, y3O, y4O, y5O);
-                        arr.push(p);
-                    }
-                    // If a program with the same name already exists, replace missing data
-                    else{
-                        setProgram(program, Name, y1,y2, y3, y4, y5, y1O, y2O, y3O, y4O, y5O);
-                    }
-                    // Reset all variables
-                    Name = "";
-                    y1 = 0;
-                    y2 = 0;
-                    y3 = 0;
-                    y4 = 0;
-                    y5 = 0;
-                    y1O = 0;
-                    y2O = 0;
-                    y3O = 0;
-                    y4O = 0;
-                    y5O = 0;
-                    flag1 = false;
-                    flag2 = false;
-                    flag3 = false;
-                    flag4 = false;
-                    flag5 = false;
+            let notReadingYears :boolean =  row[pos] !== "Upper" && row[pos]?.toLowerCase() !== "year" && row[pos]?.toLowerCase() !== "years" && row[pos] !== "All";
+            // Still adding years to a program and a new program appears.
+            if (Name !== "" && notReadingYears){
+                const program: Program | undefined = findProgram(Name.trim());
+                // Unique program, does not already exist, so create a new program and push
+                if (program === undefined){
+                    const p = new Program(Name.trim(), y1, y2, y3, y4, y5, y1O, y2O, y3O, y4O, y5O);
+                    arr.push(p);
                 }
+                else{
+                    setProgram(program, Name, y1,y2, y3, y4, y5, y1O, y2O, y3O, y4O, y5O);
+                }
+                Name = "";
+                y1 = 0;
+                y2 = 0;
+                y3 = 0;
+                y4 = 0;
+                y5 = 0;
+                y1O = 0;
+                y2O = 0;
+                y3O = 0;
+                y4O = 0;
+                y5O = 0;
+                flag1 = false;
+                flag2 = false;
+                flag3 = false;
+                flag4 = false;
+                flag5 = false;
+            }
+            // Parse a new program name
+            while (notReadingYears) {
                 Name = Name + row[pos] + " ";
                 pos ++;
+                notReadingYears = row[pos] !== "Upper" && row[pos]?.toLowerCase() !== "year" && row[pos]?.toLowerCase() !== "years" && row[pos] !== "All";
             }
 
             // Applicable years for this tuition
             while (!row[pos]!!.includes(",")){
+                // All Years
                 if (row[pos] == "All"){
                     flag1 = flag2 = flag3 = flag4 = flag5 = true;
                 }
+                // Upper years of a previously mentioned program
                 if (row[pos] == "Upper"){
                     flag2 = flag3 = flag4 = flag5 = true;
                 }
+                // Year 1
                 else if (row[pos] == '1'){
                     flag1 = true;
                 }
+                // Year 2
                 else if (row[pos] == '2'){
                     flag2 = true;
-                }else if (row[pos] == '3'){
+                }
+                // year 3
+                else if (row[pos] == '3'){
                     flag3 = true;
                 }
                 else if (row[pos] == '4'){
@@ -172,35 +192,37 @@ result.pages.forEach((page) => {
                 pos ++;
             }
 
-            // parse the tuition
+            // Flag for out of province students, it is the second price.
             var flagout : boolean = false;
+            // flag as the first price is including all add ons (bus pass etc)
             var total: boolean = true;
+            // parse the tuition, Tuition costs are in the thousands so stop at the hundreds
             while (row[pos]!!.includes(",")){
                 if (!total){
                     const price : string = row[pos]!!.replace("," , "");
                     if (flag1){
-                        if (flagout) y1O = parseInt(price);
-                        else y1 = parseInt(price);
+                        if (flagout && y1O == 0) y1O = parseInt(price);
+                        else if (y1 == 0 ) y1 = parseInt(price);
                     }
                     
                     if (flag2){
-                        if (flagout) y2O = parseInt(price);
-                        else y2 = parseInt(price);
+                        if (flagout && y2O == 0) y2O = parseInt(price);
+                        else if (y2 == 0) y2 = parseInt(price);
                     }
 
                     if (flag3){
-                        if (flagout) y3O = parseInt(price);
-                        else y3 = parseInt(price);
+                        if (flagout && y3O == 0) y3O = parseInt(price);
+                        else if (y3==0) y3 = parseInt(price);
                     }
 
                     if (flag4){
-                        if (flagout) y4O = parseInt(price);
-                        else y4 = parseInt(price);
+                        if (flagout && y4O==0) y4O = parseInt(price);
+                        else if (y4==0) y4 = parseInt(price);
                     }
 
                     if (flag5){
-                        if (flagout) y5O = parseInt(price);
-                        else y5 = parseInt(price);
+                        if (flagout && y5O == 0) y5O = parseInt(price);
+                        else if (y5 == 0) y5 = parseInt(price);
                     }
                     flagout = !flagout;
                 }
@@ -239,6 +261,7 @@ result.pages.forEach((page) => {
         }
     });
 })
+console.log(arr);
 const err = insertProgramRows("Tuition", arr);
 
 
