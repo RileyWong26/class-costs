43aea1166c75602eced748faa1d506a3720d3046
RileyWong26 <rileywongwong@gmail.com>
2026-01-09 22:40:12 -0500

    Updated the program insertion function into supabase

--- Backend/supabase.ts
+++ Backend/supabase.ts
@@ -2,20 +2,27 @@ import { SupabaseClient, createClient } from "@supabase/supabase-js";
 import 'dotenv/config';
 import { Program } from "./parser.js";
 
+// Fetch keys from env file
 const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_KEY;
+
+// Check keys exist
 if (!supabaseKey || !supabaseUrl){
     throw new Error("URL or KEY is invalid");
 }
+
+
 export const supabase = createClient(supabaseUrl, supabaseKey);
 
+
+// Insert into the database overwriting any already existing keys
 export async function insertProgramRows(Table: string, program: Array<Program>){
     const {data, error} = await supabase
     .from(Table)
-    .insert(program)
+    .upsert(program)
     
     if (error) {
-        console.log("Error " + error.message);
+        throw new Error("Error inserting into the database: " + error);
     }
     else{
         console.log("Inserted Successfully");
